2fd1e451fef4e858c2ef123ad9fb88e8b10579d3
ashwinak <ashwinak@umich.edu>
2022-11-08 12:55:37 -0500

    Added extra features

--- src/index.py
+++ src/index.py
@@ -1,8 +1,9 @@
 import os
-from flask import Flask, request, jsonify, send_file, send_from_directory
+from flask import Flask, render_template, request, redirect, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
 import mutagen
+
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///files.db"
@@ -11,7 +12,7 @@ app.config["UPLOAD_FOLDER"] = '/mnt/c/Users/Ashwin/Documents/Programming/deepgra
 db = SQLAlchemy(app)
 
 
-class Files(db.Model):
+class File(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     filename = db.Column(db.String(100))
     duration = db.Column(db.Float)
@@ -28,11 +29,12 @@ class Files(db.Model):
             'sample_rate': self.sample_rate
         }
 
+
 class FileManager:
     uid = 0
 
     @classmethod
-    def get_filename(cls, advance=True):
+    def generate_filename(cls, advance=True):
         filename = f"file{cls.uid}.wav"
         if advance:
             cls.uid += 1
@@ -42,7 +44,7 @@ class FileManager:
     def add_filename(cls, filename):
         name, ext = filename.rsplit('.', 1)
         num = 1
-        while Files.query.filter_by(filename=filename).first():
+        while File.query.filter_by(filename=filename).first():
             filename = f"{name}-{num}.{ext}"
             num += 1
         return filename
@@ -50,36 +52,23 @@ class FileManager:
 
 @app.route('/')
 def home():
-    return "<h1>Browser UI</h1>"
+    audio_files = db.session.query(File).all()
+    audio_files = [f.as_dict() for f in audio_files]
+    audio_files.sort(key=lambda x: x['filename'].lower())
+    return render_template("base.html", files=audio_files)
 
 
 @app.route('/post', methods=['POST'])
 def receive_audio():
+    app.logger.info(request.headers)
     filename = download_file()
     if filename is False:
         return "Invalid request", 400
-    
-    # Validate audio file using mutagen
-    fp = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    audio = mutagen.File(fp)
-
-    # Remove file if saved data is not a valid audio file
-    if audio is None:
-        os.remove(fp)
+    isValid = parse_and_validate_file(filename)
+    if isValid:
+        return redirect('/')
+    else:
         return "Bad audio file\n", 400
-    
-    # Add validated file with metadata to database
-    metadata = {
-        'filename': filename,
-        'duration': getattr(audio.info, 'length', None),
-        'bitrate': getattr(audio.info, 'bitrate', None),
-        'channels': getattr(audio.info, 'channels', None),
-        'sample_rate': getattr(audio.info, 'sample_rate', None)
-    }
-    info = Files(**metadata)
-    db.session.add(info)
-    db.session.commit()
-    return {"filename": filename}
 
 
 @app.route('/download')
@@ -97,8 +86,19 @@ def serve_filenames():
     args = request.args
     maxduration = args.get('maxduration', float('inf'))
     minduration = args.get('minduration', float('-inf'))
+    maxbitrate = args.get('maxbitrate', float('inf'))
+    minbitrate = args.get('minbitrate', float('-inf'))
+    channels = args.get('channels')
+    sample_rate = args.get('sample_rate')
     verbose = (args.get('verbose', False) == 'true')
-    files = Files.query.filter(Files.duration >= minduration, Files.duration <= maxduration).all()
+    query = File.query.filter(
+        File.duration >= minduration, File.duration <= maxduration,
+        File.bitrate >= minbitrate, File.bitrate <= maxbitrate)
+    if channels:
+        query = query.filter_by(channels=channels)
+    if sample_rate:
+        query = query.filter_by(sample_rate=sample_rate)
+    files = query.all()
     if verbose:
         response = {'files': [f.as_dict() for f in files]}
     else:
@@ -116,29 +116,83 @@ def serve_metadata():
     return audio_file.as_dict()
 
 
+## THIS IS ONLY HERE FOR DEMO / DEV PURPOSES -- NOT PART OF PRODUCTION
+@app.route('/clear')
+def clear_database():
+    db.session.query(File).delete()
+    db.session.commit()
+    for name in os.listdir(app.config['UPLOAD_FOLDER']):
+        fp = os.path.join(app.config['UPLOAD_FOLDER'], name)
+        os.remove(fp)
+    return redirect('/')
+## THIS IS ONLY HERE FOR DEMO / DEV PURPOSES -- NOT PART OF PRODUCTION
+
+
 def download_file():
-    '''Handles downloading POSTed file, returns filename if successful, False if unsuccessful'''
+    '''
+    Handle downloading POSTed file.
+    
+    Returns filename if successful
+    Returns False if unsuccessful'''
     contentType = request.headers.get('Content-Type')
     # curl --data-binary option used
     if contentType.startswith("application/x-www-form-urlencoded"):
-        filename = FileManager.get_filename()
-        with open(filename, 'wb') as f:
+        filename = FileManager.generate_filename()
+        fp = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        with open(fp, 'wb') as f:
             f.write(request.stream.read())
         return filename
     # curl -F option used
     elif contentType.startswith("multipart/form-data"):
         file = request.files.get('file')
         filename = FileManager.add_filename(secure_filename(file.filename))
+        if not filename:
+            filename = FileManager.generate_filename()
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         return filename
     # Invalid request
     else:
         return False
 
+
+def parse_and_validate_file(filename):
+    '''
+    Handle parsing and validating the input audio file with Mutagen.
+
+    Returns True and logs in database if file is valid
+    Returns False and deletes from memory if file is invalid
+    '''
+    # Validate audio file using mutagen
+    fp = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    audio = mutagen.File(fp)
+
+    # Remove file if saved data is not a valid audio file
+    if audio is None:
+        os.remove(fp)
+        return False
+    
+    # Add validated file with metadata to database
+    metadata = {
+        'filename': filename,
+        'duration': getattr(audio.info, 'length', None),
+        'bitrate': getattr(audio.info, 'bitrate', None),
+        'channels': getattr(audio.info, 'channels', None),
+        'sample_rate': getattr(audio.info, 'sample_rate', None)
+    }
+    info = File(**metadata)
+    db.session.add(info)
+    db.session.commit()
+    return True
+
+
 def fetch_single_query_record():
-    '''Handles fetching single record from database with name query parameter,
-    returns (name, query response)'''
+    '''
+    Handle fetching single record from database with name query parameter.
+    
+    Returns (name, query response) -- if name is not provided or file does not
+    exist in database, the corresponding element is set to None
+    '''
     args = request.args
     name = args['name'] if 'name' in args else None
-    audio_file = Files.query.filter_by(filename=name).first() if name else None
+    audio_file = File.query.filter_by(filename=name).first() if name else None
     return name, audio_file
